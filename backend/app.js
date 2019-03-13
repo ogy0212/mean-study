ed42a917ea0ee58628a328b193c811c485f59b56
ogy <ba550b0e@gmail.com>
2019-03-13 23:38:23 +0900

    3-7 connect GET posts with Angular

--- backend/app.js
+++ backend/app.js
@@ -2,13 +2,20 @@ const express = require('express');
 
 const app = express();
 
-app.use((req, res, next) => {
-  console.log('first middleware');
-  next();
-});
-
-app.use((req, res, next) => {
-  res.send('hello from express');
+app.use('/api/posts', (req, res, next) => {
+  const posts = [{
+    id: 'aseodfij',
+    title: '1 server post title',
+    content: '1 content from server'
+  }, {
+    id: 'asdfwer',
+    title: '2 server post title',
+    content: '2 content from server'
+  }];
+  res.status(200).json({
+    message: 'it is response!',
+    posts: posts
+  });
 });
 
 module.exports = app;
--- src/app/posts/posts.service.ts
+++ src/app/posts/posts.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { Subject } from 'rxjs';
 
 import { Post } from './post.model';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
@@ -10,8 +11,14 @@ export class PostsService {
   private posts: Post[] = [];
   private postsUpdated = new Subject<Post[]>();
 
+  constructor(private http: HttpClient) {}
+
   getPosts() {
-    return [...this.posts];
+    this.http.get<{message: string, posts: Post[]}>('http://localhost:3000/api/posts')
+      .subscribe((postData) => {
+        this.posts = postData.posts;
+        this.postsUpdated.next([...this.posts]);
+      });
   }
 
   getPostUpdateListener() {
@@ -20,12 +27,11 @@ export class PostsService {
 
   addPost(title: string, content: string) {
     const post: Post = {
+      id: null,
       title:  title,
       content: content
     };
     this.posts.push(post);
     this.postsUpdated.next([...this.posts]);
   }
-
-  constructor() { }
 }
