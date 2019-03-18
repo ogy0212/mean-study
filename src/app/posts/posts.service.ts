e5aff2ef1306609d04dceac2e5f2fb50baae0b57
ogy <ba550b0e@gmail.com>
2019-03-18 14:44:40 +0900

    9-12 fix id property name to corresponding to server

--- src/app/posts/posts.service.ts
+++ src/app/posts/posts.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import { Subject } from 'rxjs';
+import { map } from 'rxjs/operators';
 
 import { Post } from './post.model';
 import { HttpClient } from '@angular/common/http';
@@ -11,12 +12,22 @@ export class PostsService {
   private posts: Post[] = [];
   private postsUpdated = new Subject<Post[]>();
 
-  constructor(private http: HttpClient) {}
+  constructor(private http: HttpClient) { }
 
   getPosts() {
-    this.http.get<{message: string, posts: Post[]}>('http://localhost:3000/api/posts')
-      .subscribe((postData) => {
-        this.posts = postData.posts;
+    this.http
+      .get<{ message: string, posts: any }>('http://localhost:3000/api/posts')
+      .pipe(map((postData) => {
+        return postData.posts.map(post => {
+          return {
+            title: post.title,
+            content: post.content,
+            id: post._id
+          };
+        });
+      }))
+      .subscribe((transformedPosts) => {
+        this.posts = transformedPosts;
         this.postsUpdated.next([...this.posts]);
       });
   }
@@ -28,14 +39,14 @@ export class PostsService {
   addPost(title: string, content: string) {
     const post: Post = {
       id: null,
-      title:  title,
+      title: title,
       content: content
     };
-    this.http.post<{message: string}>('http://localhost:3000/api/posts', post)
-    .subscribe((responseData) => {
-      console.log(responseData.message);
-      this.posts.push(post);
-    this.postsUpdated.next([...this.posts]);
-    });
+    this.http.post<{ message: string }>('http://localhost:3000/api/posts', post)
+      .subscribe((responseData) => {
+        console.log(responseData.message);
+        this.posts.push(post);
+        this.postsUpdated.next([...this.posts]);
+      });
   }
 }
