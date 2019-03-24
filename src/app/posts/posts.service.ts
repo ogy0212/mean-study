439c5cbc71da27e6b377e8d6312dd7073a60d932
ogy <ba550b0e@gmail.com>
2019-03-24 20:55:41 +0900

    instant update in frontend

--- src/app/posts/posts.service.ts
+++ src/app/posts/posts.service.ts
@@ -38,7 +38,7 @@ export class PostsService {
   }
 
   getPost(id: string) {
-    return {...this.posts.find(post => post.id === id)};
+    return this.http.get<{ _id: string, title: string, content: string }>(`http://localhost:3000/api/posts/${id}`);
   }
 
   addPost(title: string, content: string) {
@@ -57,9 +57,15 @@ export class PostsService {
   }
 
   updatePost(id: string, title: string, content: string) {
-    const post: Post = { id: id, title: title, content: content};
+    const post: Post = { id: id, title: title, content: content };
     this.http.put(`http://localhost:3000/api/posts/${id}`, post)
-      .subscribe(response => console.log(response));
+      .subscribe(response => {
+        const updatedPosts = [...this.posts];
+        const oldPostIndex = updatedPosts.findIndex(p => p.id === post.id);
+        updatedPosts[oldPostIndex] = post;
+        this.posts = updatedPosts;
+        this.postsUpdated.next([...this.posts]);
+      });
   }
 
   deletePost(postId: string) {
